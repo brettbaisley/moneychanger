7cdcc797896917aeb9846fcaa5b2dda867b7b1e2
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2025-03-18 21:24:54 -0400

    Add function calling support to LLM for currency exchange conversion

--- moneychanger.py
+++ moneychanger.py
@@ -1,5 +1,6 @@
 from typing import Tuple, Dict
 import os
+import json
 from dotenv import load_dotenv
 import requests
 import streamlit as st
@@ -39,6 +40,28 @@ def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
 def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
+    
+    tools = [{
+        "type": "function",
+        "function": {
+            "name": "exchange_rate_function",
+            "description": "Convert a given amount of money from one currency to another. Each currency is represented by a 3-letter code (e.g., USD, EUR).",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "base":   {"type": "string", "description": "The base currency code (e.g., USD, EUR)"},
+                    "target": {"type": "string", "description": "The target currency code (e.g., USD, EUR)"},
+                    "amount": {"type": "string", "description": "The amount of money to convert"},
+                },
+                "required": ["base", "target", "amount"],
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    }]
+
+
+
     try:
         response = client.chat.completions.create(
             messages=[
@@ -54,14 +77,15 @@ def call_llm(textbox_input) -> Dict:
             temperature=1.0,
             top_p=1.0,
             max_tokens=1000,
-            model=model_name
+            model=model_name,
+            tools=tools
         )
         print(response.choices[0].message.content)
 
     except Exception as e:
         print(f"Exception {e} for {text}")
     else:
-        return response.choices[0].message.content
+        return response
 
 def run_pipeline():
     """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
@@ -90,4 +114,11 @@ user_input = st.text_input("Enter the amount and currency:")
 # Submit button
 if st.button("Submit"):
     # Print the contents of the text box below
-    st.write(call_llm(user_input))
+    response = call_llm(user_input)
+    response_arguments = json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+    base = response_arguments["base"]
+    target = response_arguments["target"]
+    amount = response_arguments["amount"]
+    exchange_response = get_exchange_rate(base, target, amount)
+    # Display the response
+    st.write(exchange_response)
